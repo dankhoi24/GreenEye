58988c1081c365bfc6f957493057db641c1d69d1
dankhoi24 <yedanKhoi@gmail.com>
2022-04-22 20:22:40 +0700

    Update

---
+++ GreenEye/GreenEye/DataAccess/DAO/OrderDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenEye.DataAccess.DAO
+{
+    public class OrderDAO
+    {
+        BookStoreContext Database = new BookStoreContext();
+
+        public int getCount()
+        {
+            return Database.Orders.Count();
+        }
+    }
+}

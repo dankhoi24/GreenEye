cbd187bb60286bea96e3656077a4124abf6b1709
Vodinhphuc1234 <phucatoniegriezman@gmail.com>
2022-05-04 03:16:10 +0700

    Bill View + ViewModel

--- GreenEye/GreenEye/ViewModel/FormBillManagementViewModel.cs
+++ GreenEye/GreenEye/ViewModel/FormBillManagementViewModel.cs
@@ -5,6 +5,7 @@ using GreenEye.ViewModel.Command;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,28 +34,6 @@ namespace GreenEye.ViewModel
         public ObservableCollection<Bill> BillList { get; set; }
         public NavigateStore NavigateStore { get; set; }
 
-        //Nav
-        public Bill NavSelectedBill { get; set; }
-        public ObservableCollection<Bill> NavBillList { get; set; }
-        public RelayCommand NavDeleteBillCommand { get; set; }
-        public RelayCommand NavNavigateEditBillCommand { get; set; }
-        public RelayCommand NavNavigateDetailBillCommand { get; set; }
-
-        private string _navSearchBox;
-        public string NavSearchBox
-        {
-            get => _navSearchBox; set
-            {
-                _navSearchBox = value;
-
-
-
-                onPropertyChanged(nameof(NavSearchBox));
-
-                searchNav();
-            }
-
-        }
 
         public void initPaging()
         {
@@ -75,14 +54,14 @@ namespace GreenEye.ViewModel
 
             initPaging();
 
-            AddBillNavigateCommand = new RelayCommand(AddBillNavigate, null);
+            //AddBillNavigateCommand = new RelayCommand(AddBillNavigate, null);
             NextCommand = new RelayCommand(goNext, null);
             PreviousCommand = new RelayCommand(goPrev, null);
             FirstCommand = new RelayCommand(goFirst, null);
             LastCommand = new RelayCommand(goLast, null);
 
             DeleteBillCommand = new RelayCommand(deleteBill, null);
-            NavigateEditBillCommand = new RelayCommand(navigateEditBill, null);
+            //NavigateEditBillCommand = new RelayCommand(navigateEditBill, null);
 
         }
 
@@ -94,24 +73,20 @@ namespace GreenEye.ViewModel
         private void deleteBill(object obj)
         {
             BillDAO BillDAO = new BillDAO();
-            CustomerDAO customerDAO = new CustomerDAO();
             DebitBookDAO debitBookDAO = new DebitBookDAO();
 
-           
-            
+            DebitBook debitBook = debitBookDAO.getCurrentDebitBook(SelectedBill.CustomerId);
+            debitBookDAO.increaseCurrentDebit(SelectedBill.Customer, debitBook);
 
-            BillDAO.deleteOne(NavSelectedBill);
-            BillList.Remove(NavSelectedBill);
+            BillDAO.deleteOne(SelectedBill);
+            BillList.Remove(SelectedBill);
 
-            NavBillList.Remove(NavSelectedBill);
 
             if (BillPageList.Contains(SelectedBill))
                 BillPageList.Remove(SelectedBill);
-
-
         }
 
-        private void AddBillNavigate(object obj)
+      /*  private void AddBillNavigate(object obj)
         {
             NavigateStore.CurrentViewModel = new AddNewBillViewModel(NavigateStore);
         }
@@ -120,26 +95,7 @@ namespace GreenEye.ViewModel
         {
             NavigateStore.CurrentViewModel = new AddNewBillViewModel(NavigateStore, SelectedBill);
         }
-
-        private void deleteBill(object obj)
-        {
-            BillDAO BillDAO = new BillDAO();
-            Bill_BookDAO Bill_BookDAO = new Bill_BookDAO();
-            RefundDAO refundDAO = new RefundDAO();
-
-
-            Bill_BookDAO.deleteByBill(SelectedBill.BillId);
-            refundDAO.deleteByBill(SelectedBill.BillId);
-
-
-            BillDAO.deleteOne(SelectedBill);
-            BillList.Remove(SelectedBill);
-            if (BillPageList.Contains(SelectedBill))
-                BillPageList.Remove(SelectedBill);
-
-
-        }
-
+*/
         private void goLast(object obj)
         {
             Debug.WriteLine("-------------------------------Last-----------------------------------------");
@@ -186,30 +142,6 @@ namespace GreenEye.ViewModel
             BillPageList = new ObservableCollection<Bill>(BillList.Skip((CurrentPage - 1) * itemPerPage).Take(itemPerPage));
 
             Debug.WriteLine("-------------------------------Next-----------------------------------------");
-        }
-
-        private void searchNav()
-        {
-            if (NavSearchBox != "")
-            {
-                string str = NavSearchBox;
-
-                NavBillList = new ObservableCollection<Bill>();
-
-                foreach (Bill Bill in BillList)
-                {
-                    if (str.All(char.IsDigit))
-                    {
-                        if (Bill.BillId == Int16.Parse(str))
-                            NavBillList.Add(Bill);
-                    }
-                    else if (Bill.Customer.Name.Contains(str.ToLower()))
-                    {
-                        NavBillList.Add(Bill);
-                    }
-                }
-            }
-
         }
 
     }

e194db4612d56b54157ef4ae94e0776f3868eb2a
dankhoi24 <yedanKhoi@gmail.com>
2022-05-04 21:36:24 +0700

    Update BillDAO

---
+++ GreenEye/GreenEye/DataAccess/DAO/BillDAO.cs
@@ -0,0 +1,26 @@
+﻿using GreenEye.DataAccess.Domain;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenEye.DataAccess.DAO
+{
+    public class BillDAO
+    {
+          private BookStoreContext Database = new BookStoreContext();
+
+        internal ObservableCollection<Bill> getAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void deleteOne(Bill navSelectedBill)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
